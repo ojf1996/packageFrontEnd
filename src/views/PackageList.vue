649c03834b51f03a7eb0c9b14e888606ddb04854
oujianfeng <247057911@qq.com>
2019-07-25 19:31:31 +0800

    not show confirm button when package has been fetched

--- src/views/PackageList.vue
+++ src/views/PackageList.vue
@@ -2,7 +2,7 @@
   <div class="package-list">
     <a-table :dataSource="packages" :columns="columns" :loading="loading" rowKey="id">
       <template slot="operation" slot-scope="text, record">
-        <a-button v-on:click="fetchPackage(record)">确认收货</a-button>
+        <a-button v-if="recode.status == 'Fetched'" v-on:click="fetchPackage(record)">确认收货</a-button>
       </template>
     </a-table>
   </div>
