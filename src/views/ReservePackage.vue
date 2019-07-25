86dc30b8911e6b85689df87d7d63b0279afea24f
oujianfeng <247057911@qq.com>
2019-07-25 20:01:01 +0800

    modify reserve package style

--- src/views/ReservePackage.vue
+++ src/views/ReservePackage.vue
@@ -1,36 +1,41 @@
 <template>
-  <div class="package-adder">
-    <a-form
-      :form="form"
-      @submit="handleSubmit"
-    >
-      <a-form-item v-bind="formItemLayout" label="运单号">
-        <a-input
-         v-decorator="['id']"
-        />
-      </a-form-item>
-      <a-form-item v-bind="formItemLayout" label="取件时间">
-        <a-date-picker
-          v-decorator="['time']"
-          show-time
-          format="YYYY-MM-DD HH:mm:ss"
-        />
-      </a-form-item>
-      <a-form-item
-        :wrapper-col="{
-          xs: { span: 24, offset: 0 },
-          sm: { span: 16, offset: 8 },
-        }"
+  <a-layout style="width:inhert;">
+    <a-layout-header style="width:inhert; background:transparent;">
+      预约取件
+    </a-layout-header>
+    <div class="package-adder">
+      <a-form
+        :form="form"
+        @submit="handleSubmit"
       >
-        <a-button type="primary" html-type="submit">
-          预约
-        </a-button>
-        <a-button type="danger" html-type="reset" style="margin-left: 5px;">
-          取消
-        </a-button>
-      </a-form-item>
-    </a-form>
-  </div>
+        <a-form-item v-bind="formItemLayout" label="运单号">
+          <a-input
+          v-decorator="['id']"
+          />
+        </a-form-item>
+        <a-form-item v-bind="formItemLayout" label="取件时间">
+          <a-date-picker
+            v-decorator="['time']"
+            show-time
+            format="YYYY-MM-DD HH:mm:ss"
+          />
+        </a-form-item>
+        <a-form-item
+          :wrapper-col="{
+            xs: { span: 24, offset: 0 },
+            sm: { span: 16, offset: 8 },
+          }"
+        >
+          <a-button type="primary" html-type="submit">
+            预约
+          </a-button>
+          <a-button type="danger" html-type="reset" style="margin-left: 5px;">
+            取消
+          </a-button>
+        </a-form-item>
+      </a-form>
+    </div>
+  </a-layout>
 </template>
 
 <script>
