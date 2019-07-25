29a7ed62dc619d5e1c08893c2de68d9b87d55188
Jimmy Ou <jimmy.ou@oocl.com>
2019-07-25 12:21:01 +0800

    add sidebar components

---
+++ src/components/Home/sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <a-menu class="sidebar">
+    <a-menu-item key="1">
+      <router-link to="/" >home</router-link>
+    </a-menu-item>
+    <a-menu-item key="2">
+      <router-link to="/">add package</router-link>
+    </a-menu-item>
+    <a-menu-item key="3">
+      <router-link to="/">reserve package</router-link>
+    </a-menu-item>
+  </a-menu>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+.sidebar {
+  height: 100%;
+}
+</style>
