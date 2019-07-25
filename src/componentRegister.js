24ed41d78e10735e9de9c05dbfde86b2820595ea
Jimmy Ou <jimmy.ou@oocl.com>
2019-07-25 12:19:59 +0800

    adding ant-design components

---
+++ src/componentRegister.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import 'ant-design-vue/dist/antd.css'
+
+import { Input, Button, Avatar, Menu, Layout, List } from 'ant-design-vue'
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Avatar)
+Vue.use(Menu)
+Vue.use(Layout)
+Vue.use(List)
